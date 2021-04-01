4edc56d41ca230584663a7bd322ffd5a35dc1393
Lucas <lucas@pop-os.localdomain>
2021-04-01 16:06:09 -0300

    Correção de Routes

--- chat_audio/projeto/__init__.py
+++ chat_audio/projeto/__init__.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from projeto import app
+from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.coonfig['SECRET_KEY'] = ''
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-db = SQLAlchemy(app)
+#app.coonfig['SECRET_KEY'] = ''
+#app.config['SQLALCHEMY_DATABASE_URI'] = ''
+#db = SQLAlchemy(app)
 
 from projeto import routes
--- chat_audio/projeto/routes.py
+++ chat_audio/projeto/routes.py
@@ -5,15 +5,15 @@ from projeto import app
 @app.route('/')
 @app.route('/login')
 def login():
-    return render_template(url_for('login'), title='Login')
+    return render_template('login.html', title='Login')
 
 @app.route('/cadastro')
 def cadastro():
-    return render_template(url_for('cadastro'), title='Cadastro')
+    return render_template('cadastro.html', title='Cadastro')
 
 @app.route('/chat')
 def chat():
-    return render_template(url_for('chat'))
+    return render_template('chat.html', title='Chat')
 
 @app.route('/sobre')
 def sobre():
