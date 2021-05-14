308bfb0bd730755fb762b225db8e6ae778631cc7
Lucas <lucas@pop-os.localdomain>
2021-05-14 11:34:22 -0300

    Banco de Dados

--- chat_audio/projeto/__init__.py
+++ chat_audio/projeto/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
-#from flask_mysqldb import MySQL
-from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
@@ -14,8 +13,8 @@ app.config['SECRET_KEY'] = '8ba8d1387bcb2460b0a5cc0831c0db65'
 app.config['MYSQL_HOST']= 'localhost'
 app.config['MYSQL_USER']= 'root'
 app.config['MYSQL_PASSWORD']= ''
-#app.config['MYSQL_DB']= ''
-#app.config['MYSQL_CURSOR']= 'Disct'
+app.config['MYSQL_DB']= ''
+app.config['MYSQL_CURSOR']= 'Disct'
 
 #login_manager = LoginManager(app)
 #login_manager.login_view = 'login'
--- chat_audio/projeto/models.py
+++ chat_audio/projeto/models.py
@@ -1,6 +1,18 @@
 from projeto import db, login_manager
+from projeto import db
 
 
 @login_manager.user_loader
 def load_user(id_usuario):
     return User.query.get(int(user_id))
+
+def funcao():
+    cur =db.connection.cursor()
+    cur.execute('''
+        INSERT INTO cadastro ('email', 'senha') VALUES ('lucas@gmail.com', '123'), ('gabriel@gmail.com', '123'),
+    ('gustavo@gmail.com', '123');
+
+    INSERT INTO gravando ('id_audio', 'gravador') VALUES (1, 'teste de gravação'), (2, 'teste de gravação 2'),
+    (3, 'teste de gravação 3');
+        
+        ''')
